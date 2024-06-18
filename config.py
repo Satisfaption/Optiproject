80e4d7e91d199d032607324d37003d4dad8efd67
Satisfaption <buchmuerik@gmail.com>
2024-06-18 03:44:41 +0200

    config

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 MONGODB_URI_TEMPLATE = "mongodb+srv://{username}:{password}@opticluster.gym8xek.mongodb.net/"
 MONGODB_URI_GUEST = "mongodb+srv://Guest:DpPh0Lh2ilEFoIr2@opticluster.gym8xek.mongodb.net/"
 MONGODB_NAME = 'Optigrün'
-
