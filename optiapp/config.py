cc1b17195a4de5642185f795062e78672abbd3d2
Satisfaption <buchmuerik@gmail.com>
2024-06-26 17:41:17 +0200

    restructure

--- optiapp/config.py
+++
@@ -1,9 +0,0 @@
-MONGODB_URI_TEMPLATE = "mongodb+srv://{username}:{password}@opticluster.gym8xek.mongodb.net/"
-MONGODB_URI_GUEST = "mongodb+srv://Guest:DpPh0Lh2ilEFoIr2@opticluster.gym8xek.mongodb.net/"
-MONGODB_NAME = 'Optigrün'
-
-OPENROUTE_API_KEY = '5b3ce3597851110001cf62489a6945fd9904480090dc413078afb8b2'
-
-new_version = "1.0.1"
-filename = "Optigelb-Setup"
-download_url = f"https://github.com/Satisfaption/Optiproject/releases/download/v{new_version}/{filename}-{new_version}.exe"
